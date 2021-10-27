c62e7ffe07604fbf38215072007768ea12e135c9
ceo991 <ceo991@hotmail.com>
2021-10-27 10:49:21 +0300

    Vue App created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppMain/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import AppMain from './components/AppMain.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppMain
   }
 }
 </script>
@@ -23,6 +23,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  //margin-top: 60px;
 }
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="wrapper">
+        <!--<div class="card">
+            <h1 class="font-bold text-6xl">Vues With TailwindCSS</h1>
+            <h2 class="text-3xl font-bold">Using VueCLI</h2>
+        </div>-->
+<table class="table-fixed">
+  <tbody>
+    <tr>
+      <td class="border border-pink-500 p-8"><button>O</button></td>
+      <td class="border border-pink-500 p-8"><button>X</button></td>
+      <td class="border border-pink-500 p-8"><button>X</button></td>
+    </tr>
+    <tr>
+      <td class="border border-pink-500 p-8"><button>X</button></td>
+      <td class="border border-pink-500 p-8"><button>O</button></td>
+      <td class="border border-pink-500 p-8"><button>O</button></td>
+    </tr>
+    <tr>
+      <td class="border border-pink-500 p-8"><button>X</button></td>
+      <td class="border border-pink-500 p-8"><button>O</button></td>
+      <td class="border border-pink-500 p-8"><button>X</button></td>
+
+    </tr>
+
+  </tbody>
+</table> 
+        
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="postcss" scoped>
+    .wrapper{
+        @apply flex justify-center items-center h-screen text-emerald-500 text-center bg-gray-100 ;
+    }
+    .card{
+        @apply rounded-lg border border-emerald-300 bg-emerald-100 p-10 shadow-lg;
+    }
+
+</style>
