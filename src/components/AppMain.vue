bd6195ac52683f34690518e85b7f13bbffc1d6b7
DESKTOP-EUKB5FE\ceo99 <33651801+ceo991@users.noreply.github.com>
2021-10-30 17:49:18 +0300

    some features added

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,9 +1,5 @@
 <template>
     <div class="wrapper">
-        <!--<div class="card">
-            <h1 class="font-bold text-6xl">Vues With TailwindCSS</h1>
-            <h2 class="text-3xl font-bold">Using VueCLI</h2>
-        </div>-->
     
         <h1 class="text-3xl block card">{{player}}</h1>
         <h1 class="text-3xl block card">{{playerWinState}}</h1>
@@ -29,6 +25,19 @@
 
   </tbody>
 </table> 
+
+ <transition name="fade" appear>
+  <div class="modal-overlay" v-if="isGameEnded" @click="showModal = false"></div>
+ </transition>
+ <transition name="slide" appear>
+  <div class="modal" v-if="isGameEnded">
+   <h1>GAME OVER !</h1>
+   <p>{{playerWinState}}</p>
+   <button class="button" @click="restartGame()">
+    Restart
+   </button>
+  </div>
+ </transition>
 
     </div>
 </template>
@@ -156,6 +165,38 @@
               this.isGameEnded = true;
               this.player = "";
             }
+          },
+          restartGame(){
+            this.isGameEnded= false;
+            this.playerWinState= "";
+            document.getElementById(1).innerText = "";
+            document.getElementById(2).innerText = "";
+            document.getElementById(3).innerText = "";
+            document.getElementById(4).innerText = "";
+            document.getElementById(5).innerText = "";
+            document.getElementById(6).innerText = "";
+            document.getElementById(7).innerText = "";
+            document.getElementById(8).innerText = "";
+            document.getElementById(9).innerText = "";
+
+            document.getElementById(1).style.color = "inherit";
+            document.getElementById(2).style.color = "inherit";
+            document.getElementById(3).style.color = "inherit";
+            document.getElementById(1).style.fontWeight = "normal";
+            document.getElementById(2).style.fontWeight = "normal";
+            document.getElementById(3).style.fontWeight = "normal";
+            document.getElementById(4).style.color = "inherit";
+            document.getElementById(5).style.color = "inherit";
+            document.getElementById(6).style.color = "inherit";
+            document.getElementById(4).style.fontWeight = "normal";
+            document.getElementById(5).style.fontWeight = "normal";
+            document.getElementById(6).style.fontWeight = "normal";
+            document.getElementById(7).style.color = "inherit";
+            document.getElementById(8).style.color = "inherit";
+            document.getElementById(9).style.color = "inherit";
+            document.getElementById(7).style.fontWeight = "normal";
+            document.getElementById(8).style.fontWeight = "normal";
+            document.getElementById(9).style.fontWeight = "normal";
           }
         },
         updated () {
@@ -171,5 +212,106 @@
     .card{
         @apply rounded-lg border border-emerald-300 bg-emerald-100 p-10 shadow-lg;
     }
+
+
+
+body {
+ font-family: 'montserrat', sans-serif;
+}
+
+#app {
+ position: relative;
+ 
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ 
+ width: 100vw;
+ min-height: 100vh;
+ overflow-x: hidden;
+}
+
+.button {
+ appearance: none;
+ outline: none;
+ border: none;
+ background: none;
+ cursor: pointer;
+ 
+ display: inline-block;
+ padding: 15px 25px;
+ background-image: linear-gradient(to right, #CC2E5D, #FF5858);
+ border-radius: 8px;
+ 
+ color: #FFF;
+ font-size: 18px;
+ font-weight: 700;
+ 
+ box-shadow: 3px 3px rgba(0, 0, 0, 0.4);
+ transition: 0.4s ease-out;
+ 
+ &:hover {
+  box-shadow: 6px 6px rgba(0, 0, 0, 0.6);
+ }
+}
+
+.modal-overlay {
+ position: absolute;
+ top: 0;
+ left: 0;
+ right: 0;
+ bottom: 0;
+ z-index: 98;
+ background-color: rgba(0, 0, 0, 0.3);
+}
+
+.modal {
+ position: fixed;
+ top: 50%;
+ left: 50%;
+ transform: translate(-50%, -50%);
+ z-index: 99;
+ 
+ width: 100%;
+ max-width: 400px;
+ background-color: #FFF;
+ border-radius: 16px;
+ 
+ padding: 25px;
+ 
+ h1 {
+  color: #222;
+  font-size: 32px;
+  font-weight: 900;
+  margin-bottom: 15px;
+ }
+ 
+ p {
+  color: #666;
+  font-size: 18px;
+  font-weight: 400;
+  margin-bottom: 15px;
+ }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+ transition: opacity .5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+ opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+ transition: transform .5s;
+}
+
+.slide-enter,
+.slide-leave-to {
+ transform: translateY(-50%) translateX(100vw);
+}
 
 </style>
